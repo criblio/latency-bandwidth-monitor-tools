d31229fcc9652b54b1484b06791c7db22ae7443d
Clint Sharp <clint@cribl.io>
2021-07-05 21:14:28 -0700

    mtr to dynamic link

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 ORIG_DIR=$(pwd)
 TMP_DIR=$(mktemp -d)
@@ -7,14 +7,15 @@ curl -Lo ./mtr.tar.gz https://github.com/traviscross/mtr/archive/refs/tags/v0.94
 tar zxvf ./mtr.tar.gz
 cd mtr*
 ./bootstrap.sh
-./configure CFLAGS="-static"
-make LDFLAGS="-static"
+./configure 
+make mtr_LDADD="-Wl,-Bstatic -ljansson -Wl,-Bdynamic -lresolv -lm"
 cp ./mtr $ORIG_DIR
 cd ..
 rm -rf mtr*
 cd $ORIG_DIR
 
-go get github.com/criblio/speedtest-go
-cd ~/go/src/github.com/criblio/speedtest-go
+export GO111MODULE=on
+GOPROXY=direct go get github.com/criblio/speedtest-go
+cd ~/go/pkg/mod/github.com/criblio/speedtest-go\@*
 CGO_ENABLED=0 go build -tags netgo -o $ORIG_DIR/speedtest
 cd $ORIG_DIR
